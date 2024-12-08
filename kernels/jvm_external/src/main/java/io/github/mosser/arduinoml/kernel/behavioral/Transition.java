b331526e5c68136720d5046913cc7aa8d93d2121
Julien D. <juliendidier83550@gmail.com>
2024-12-08 20:16:24 +0100

    antlr4: add / or

--- kernels/jvm_external/src/main/java/io/github/mosser/arduinoml/kernel/behavioral/Transition.java
+++ kernels/jvm_external/src/main/java/io/github/mosser/arduinoml/kernel/behavioral/Transition.java
@@ -2,7 +2,6 @@ package io.github.mosser.arduinoml.kernel.behavioral;
 
 import io.github.mosser.arduinoml.kernel.generator.Visitable;
 import io.github.mosser.arduinoml.kernel.generator.Visitor;
-import io.github.mosser.arduinoml.kernel.structural.*;
 
 public abstract class Transition implements Visitable {
 
--- kernels/jvm_external/src/main/java/io/github/mosser/arduinoml/kernel/generator/Visitor.java
+++ kernels/jvm_external/src/main/java/io/github/mosser/arduinoml/kernel/generator/Visitor.java
@@ -1,8 +1,12 @@
 package io.github.mosser.arduinoml.kernel.generator;
 
-import io.github.mosser.arduinoml.kernel.behavioral.*;
-import io.github.mosser.arduinoml.kernel.structural.*;
 import io.github.mosser.arduinoml.kernel.App;
+import io.github.mosser.arduinoml.kernel.behavioral.Action;
+import io.github.mosser.arduinoml.kernel.behavioral.SignalTransition;
+import io.github.mosser.arduinoml.kernel.behavioral.State;
+import io.github.mosser.arduinoml.kernel.behavioral.TimeTransition;
+import io.github.mosser.arduinoml.kernel.structural.Actuator;
+import io.github.mosser.arduinoml.kernel.structural.Sensor;
 
 import java.util.HashMap;
 import java.util.Map;

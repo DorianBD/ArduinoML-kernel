2560120eeb7e8ea88371838873fea6932f29b08a
Benjamin Neuville <benjamin.neuville-burguiere@etu.unice.fr>
2024-12-08 16:49:32 +0100

    end groovy & deleting useless langage

---
+++ embeddeds/groovy/GroovuinoML/scripts/ScenarioTemporalCondition.groovy
@@ -0,0 +1,12 @@
+sensor "bouton" onPin 9
+actuator "led" pin 12
+
+state "on" means "led" becomes "high"
+state "off" means led becomes low
+
+initial "off"
+
+from off to on when bouton becomes high end
+from on to off after 800 end
+
+export "Switch!"
